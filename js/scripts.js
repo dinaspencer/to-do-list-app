6a405b568632a4818a4352d193040684e46a7e47
Dina Spencer <dkay22@gmail.com>
2022-08-11 12:52:06 +0200

    Formatted JS file with Prettier / ESLint

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,40 @@
-function newItem (){
+function newItem() {
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+  let list = $("#list");
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
-let list = $('#list');
-
-if (inputValue === ''){
-  alert("Please add an item to the list.");
-} else {
-  list.append(li);
+  if (inputValue === "") {
+    alert("Please add an item to the list.");
+  } else {
+    list.append(li);
   }
 
-//cross out items when done by double clicking
+  //cross out items when done by double clicking
   function crossOut() {
-    li.toggleClass('strike');
+    li.toggleClass("strike");
   }
-  li.on('dblclick', function crossOut(){
-    li.toggleClass('strike');
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
   });
 
-//to remove text from input area after it's been added to the list
+  //to remove text from input area after it's been added to the list
   function emptyInput() {
-    $('input').val('');
+    $("input").val("");
   }
-    $('input').on('click', emptyInput);
+  $("input").on("click", emptyInput);
 
-//adding functionality to delete items from list
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X'));
+  //adding functionality to delete items from list
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
   li.append(crossOutButton);
 
   function deleteListItem() {
-    li.addClass('delete');
+    li.addClass("delete");
   }
 
-  crossOutButton.on('click', deleteListItem);
+  crossOutButton.on("click", deleteListItem);
 
-//making the list sortable with built in function
+  //making the list sortable with built in function
   list.sortable();
-
 }
